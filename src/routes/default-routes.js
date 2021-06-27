ccb041031a03076bbb91a3336475dd0f55a25cd1
Neandher Carlos <neandher89@gmail.com>
2021-06-27 19:12:29 -0300

    teste ci cd

--- src/routes/default-routes.js
+++ src/routes/default-routes.js
@@ -3,7 +3,7 @@ let router = require('express').Router();
 router.get('/', function (req, res) {
   res.json({
     status: 'ok',
-    message: 'Servidor rodando perfeitamenteeeee',
+    message: 'Servidor rodando perfeitamenteeeee____',
   });
 });
 
